3669d0306f271f286acdbf225f344763e942ddcd
Ronald LIOU-KEE-ON <rliou-ke@e1r11p22.42.fr>
2017-01-23 18:58:42 +0100

    structure tetri

--- fillit.py
+++ fillit.py
@@ -10,41 +10,54 @@ def exit_error(error_message):
     print error_message
     exit(1)
 
+def split(arr, size):
+	arrs = []
+	while len(arr) > size:
+		pice = arr[:size]
+		arrs.append(pice)
+		arr = arr[size:]
+	arrs.append(arr)
+	return arrs
+
 length = len(sys.argv)
 if length < 2:
-    exit_error("No file input")
+	exit_error("Error: No file input")
 elif length > 2:
     print "Warning: Too much arguments. Ignoring all but the first argument."
 
 try :
     file = open(sys.argv[1], 'r')
 except IOError:
-    print "Invalid file"
-    exit(1)
+	exit_error("Error: Invalid file")
 lines = file.readlines()
 file.close()
 cleaned = []
 for u in lines:
-    if (not special_match(u) and len(u) == 5) or u == "\n":
-        cleaned.append(u)
-    else:
-        exit_error("Error")
+	if not special_match(u) and len(u) == 5:
+		cleaned.append(u.translate(None, '\n'))
+	elif u == "\n":
+		cleaned.append(u)
+	else:
+		exit_error("Error")
 
 alone = False
+length = 0
 for i in cleaned:
-    if not i == "\n":
-        alone = True
-        for u in i:
-            if u == '\n':
-                print "end"
-    else:
-        if alone == True:
-            alone = False
-        else :
-            print "Error: Too much newline(s)"
-            exit(1)
+	if not i == "\n":
+		alone = True
+#		for u in i:
+#			if u == '\n':
+#				print "end"
+	else:
+		if alone == True and length == 4:
+			alone = False
+			length = -1
+		else :
+			exit_error("Error: Too much newline(s)")
+	length = length + 1
 if i == "\n":
-    print "Error: Newline excess at EOF"
-    exit(1)
-
-
+    exit_error("Error: Newline excess at EOF")
+print cleaned
+cleaned = filter(lambda i: i != "\n", cleaned)
+tetriminos = split(cleaned, 4)
+print tetriminos
